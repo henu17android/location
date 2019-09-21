6809ea10fbbcb280a3ea0ef8fa7dafc13210637b
祁梦楠 <1710121104@vip.henu.edu.cn>
2019-09-21 08:46:59 +0800

    drawerlayout

--- app/src/main/java/com/example/location/MainActivity.java
+++ app/src/main/java/com/example/location/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.location;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 }
